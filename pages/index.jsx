faad9b06639fe267f76860e936ae10130aae777e
Liam <liam.aiello@mail.utoronto.ca>
2021-11-11 22:29:58 -0500

    Add new CSS to NavBar and global background.

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/home.module.css';
 const home = () => {
   return(
   <div className={styles.home}>
-    <h1>Welcome</h1>
+    <h1>Welcome!</h1>
     <p>I am Liam Aiello, pleased to meet you!</p>
     <Button path="about">About</Button>
   </div>
